dcbc93f4bf447b6a362909a95be050b405845f09
Triệu Tấn Đạt <dattantrieu20042@gmail.com>
2025-04-25 15:49:18 +0700

    add root

--- app/main.py
+++ app/main.py
@@ -5,11 +5,14 @@ import os
 from app.utils import search_image
 
 app = FastAPI()
+@app.get("/")
+def root():
+    return {"message": "Server is running."}
 
 @app.post("/search")
 async def search(file: UploadFile = File(...), top_k: int = 5):
     # Lưu ảnh tạm thời
-    temp_path = f"temp/{file.filename}"
+    temp_path = f"/tmp/{file.filename}"
     os.makedirs("temp", exist_ok=True)
     with open(temp_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
@@ -21,3 +24,6 @@ async def search(file: UploadFile = File(...), top_k: int = 5):
         return results_df.to_dict(orient="records")
     except Exception as e:
         return JSONResponse(status_code=500, content={"error": str(e)})
+
+# uvicorn app.main:app --host 0.0.0.0 --port 8000
+# Truy cập API tại: http://localhost:8000/docs để test giao diện Swagger UI.
